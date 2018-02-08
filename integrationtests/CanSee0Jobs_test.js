e81e98f43dd528eec5abefabc4dac7ceb976e537
Robert Metcalf <rmetcalf9@googlemail.com>
2018-02-08 10:29:12 +0000

    Adding wait to test case

--- integrationtests/CanSee0Jobs_test.js
+++ integrationtests/CanSee0Jobs_test.js
@@ -3,6 +3,7 @@ Feature('CanSee0Jobs');
 
 Scenario('I can see 0 Jobs', (I) => {
   I.amOnPage('/');
+  I.waitForElement('DIV.layout DIV.layout-page-container .layout-page', 3000); // Wait for 3 seconds
   within('DIV.layout DIV.layout-page-container .layout-page', () => {
     I.see('Total jobs setup: 0');
   });
