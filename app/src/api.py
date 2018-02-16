ef4bfea315b8e32cf38f5eb7ddae80e1e41529dd
Robert Metcalf <rmetcalf9@googlemail.com>
2018-02-16 16:21:08 +0000

    Switching swagger framework to try and get it all working under apidocs

--- app/src/api.py
+++ app/src/api.py
@@ -1,6 +1,6 @@
 from flask import Flask, Blueprint
-from flasgger import Swagger, swag_from
 from appObj import appObjClass
+from flask_restplus import Api, Resource, fields, apidoc
 import datetime
 import pytz
 app = Flask(__name__)
@@ -10,8 +10,26 @@ from webfrontendAPI import webfrontendBP
 
 appObj = appObjClass()
 
+# I need to subclass this in order to change the url_prefix for swaggerui
+#  so I can reverse proxy everyting under /apidocs
+class SubclassRestPlusAPI(Api):
+  def __init__(self, *args, reverse=False, **kwargs):
+      super().__init__(*args, **kwargs)
+  def _register_apidoc(self, app):
+    conf = app.extensions.setdefault('restplus', {})
+    if not conf.get('apidoc_registered', False):
+      app.register_blueprint(apidoc.apidoc, url_prefix='/apidocs')
+    conf['apidoc_registered'] = True
+
+
+api = SubclassRestPlusAPI(app, version='1.0', title='TodoMVC API',
+    description='A simple TodoMVC API', doc='/'
+)
+apidocs_blueprint = Blueprint('api', __name__)
+api.init_app(apidocs_blueprint)  
+app.register_blueprint(apidocs_blueprint, url_prefix='/apidocs')
+
 @bp.route('/serverinfo')
-@swag_from('./endpoints/serverinfo.yml')
 def serverinfo():
   curDatetime = datetime.datetime.now(pytz.utc)
   return appObj.getServerInfoJSON(curDatetime)
--- app/src/app.py
+++ app/src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, Blueprint, jsonify, request
-from flasgger import Swagger
 from api import app
+from flask_restplus import Api, Resource, fields
 import signal
 import sys
 import os
@@ -34,48 +34,6 @@ try:
     raise Exception('Wrong number of paramaters passed (Got ' + str((len(sys.argv)-1)) + " expected " + str(expectedNumberOfParams) + ")")
   GlobalParamaters.set(GlobalParamatersClass(os.environ))
   print(GlobalParamaters.get().getStartupOutput())
-
-  # Make app support /apidocs for swaggerUI
-  swaggerTemplate = {
-    "swagger": "2.0",
-    "info": {
-      "title": "DockJob API",
-      "description": "API for DockJob",
-      "contact": {
-        "responsibleOrganization": "ME",
-        "responsibleDeveloper": "Me",
-        # "email": "rmetcalf9@googlemail.com",
-        "url": "https://github.com/rmetcalf9/dockJob",
-      },
-      "termsOfService": "http://me.com/terms",
-      "version": GlobalParamaters.get().version
-    },
-    "host": GlobalParamaters.get().getAPIHost(),  # overrides localhost:500
-    #basePath dosen't seem to be used for registering the blueprint, but it is used for internal links
-    "basePath": GlobalParamaters.get().getAPIPath(),  # base bash for blueprint registration
-    "schemes": [
-      "http",
-      "https"
-    ]
-  }
-  swaggerConfig = {
-    "headers": [
-    ],
-    "specs": [
-        {
-            "endpoint": 'apispec_1',
-            "route": '/apispec_1.json',
-            "rule_filter": lambda rule: True,  # all in
-            "model_filter": lambda tag: True,  # all in
-        }
-    ],
-    "static_url_path": "/flasgger_static",
-    # "static_folder": "static",  # must be set by user
-    "swagger_ui": True,
-    "specs_route": "/apidocs/"
-  }
-
-  swagger = Swagger(app, template=swaggerTemplate, config=swaggerConfig)
 
   app.run(host='0.0.0.0', port=80, debug=False)
 except ServerTerminationError as e:
