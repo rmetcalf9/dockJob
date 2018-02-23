70949d98e6da72a6cd827aad4963d3dcdf5915d5
Robert Metcalf <rmetcalf9@googlemail.com>
2018-02-23 14:05:07 +0000

    Flask API Test

--- app/test/test_api.py
+++ app/test/test_api.py
@@ -1,18 +1,44 @@
+from TestHelperSuperClass import testHelperSuperClass
 import unittest
 from appObj import appObj
 appObj.init()
 import api
- 
-class test_api(unittest.TestCase):
+import json
+from GlobalParamaters import GlobalParamaters, GlobalParamatersClass
 
+env = {
+  'APIAPP_MODE': 'DOCKER',
+  'APIAPP_VERSION': 'TEST-3.3.3',
+  'APIAPP_FRONTEND': '../app',
+  'APIAPP_APIURL': 'http://apiurlxxx',
+  'APIAPP_APIACCESSSECURITY': '[{ "type": "basic-auth" }]',
+}
+
+
+class test_api(testHelperSuperClass):
+  testClient = None
   def setUp(self):
-    appObj.init()
+    GlobalParamaters.set(GlobalParamatersClass(env))
+    self.testClient = appObj.flaskAppObject.test_client()
+    self.testClient.testing = True 
   def tearDown(self):
-    pass
+    self.testClient = None
 
-    def test_getServceInfo(self):
-        #calc = Calculator()
-        #result = calc.add(2,2)
-        #self.assertEqual(6, result)
-        pass
+  def test_getServceInfo(self):
+    expRes = json.dumps({
+      'Jobs': {
+        'NextExecuteJob': None,
+        'TotalJobs': 0
+      },
+      'Server': {
+        'DefaultUserTimezone': 'Europe/London', 
+        'ServerDatetime': 'IGNORE'
+      },
+      })
+    result = self.testClient.get('/api/serverinfo/')
+    self.assertEqual(result.status_code, 200)
+    resultJSON = json.loads(result.get_data(as_text=True))
+    resultJSON['Server']['ServerDatetime'] = 'IGNORE'
+    self.assertJSONStringsEqual(resultJSON, json.loads(expRes));
+    pass
 
