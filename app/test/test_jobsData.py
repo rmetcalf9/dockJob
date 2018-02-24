4fb9ae26c3aacb9976a80fc736d2d72d2a3b4fce
Robert Metcalf <rmetcalf9@googlemail.com>
2018-02-24 08:50:03 +0000

    Refactoring to move iso function

--- app/test/test_jobsData.py
+++ app/test/test_jobsData.py
@@ -2,22 +2,11 @@ from TestHelperSuperClass import testHelperAPIClient
 
 from jobsData import t
 import json
-import pytz
-import dateutil.parser
-
+from utils import from_iso8601
 import api
 
 
 class test_jobsData(testHelperAPIClient):
-  def from_iso8601(self, when=None, tz=pytz.timezone("UTC")):
-    _when = dateutil.parser.parse(when)
-    if not _when.tzinfo:
-      _when = tz.localize(_when)
-    print(_when.tzinfo)
-    if (str(_when.tzinfo) != 'tzutc()'):
-      raise Exception('Error - Only conversion of utc times from iso8601 is supported')
-    return _when
-
   def test_JobCreate(self):
     params = {
       "name": "TestJob",
@@ -40,7 +29,7 @@ class test_jobsData(testHelperAPIClient):
     resultJSON = json.loads(result.get_data(as_text=True))
     self.assertTrue(len(resultJSON['guid']) == 36, msg='Invalid GUID - must be 36 chars')
     self.assertTrue(resultJSON['creationDate'] == resultJSON['lastUpdateDate'], msg='Creation date dosen''t match last update')
-    tim = self.from_iso8601(resultJSON['creationDate'])
+    tim = from_iso8601(resultJSON['creationDate'])
     self.assertTimeCloseToCurrent(tim)
     resultJSON['guid'] = expRes['guid']
     resultJSON['creationDate'] = expRes['creationDate']
