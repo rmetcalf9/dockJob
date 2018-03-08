3404e187e4e36d488b97421ef4e8f5943ed41732
Robert Metcalf <rmetcalf9@googlemail.com>
2018-03-08 12:51:58 +0000

    Impersonation test

--- app/test/test_JobExecutor.py
+++ app/test/test_JobExecutor.py
@@ -10,7 +10,6 @@ class test_appObjClass(testHelperAPIClient):
 
   def test_ExecuteAsThisUser(self):
     thisUID = os.getuid()
-    thisGID = os.getgid()
     cmdToExecute = 'id -u'
     expResSTDOUT = str(thisUID) + '\n'
     res = appObj.jobExecutor.executeCommand(cmdToExecute)
@@ -18,7 +17,6 @@ class test_appObjClass(testHelperAPIClient):
 
   def test_ErrorSTDErrOutput(self):
     thisUID = os.getuid()
-    thisGID = os.getgid()
     cmdToExecute = 'sadgfdhgf'
     expResSTDOUT = '/bin/sh: 1: sadgfdhgf: not found\n'
     res = appObj.jobExecutor.executeCommand(cmdToExecute)
@@ -26,8 +24,25 @@ class test_appObjClass(testHelperAPIClient):
 
   def test_MultipleCommandsWithMixedOutput(self):
     thisUID = os.getuid()
-    thisGID = os.getgid()
     cmdToExecute = 'echo "Hello"\newflkjdsfdsjlk\necho "Goodbye"'
     expResSTDOUT = 'Hello\n/bin/sh: 2: ewflkjdsfdsjlk: not found\nGoodbye\n'
     res = appObj.jobExecutor.executeCommand(cmdToExecute)
     self.assertEqual(res.stdout.decode(), expResSTDOUT)
+
+  def test_ExecuteAsDockJobUserUser(self):
+    env = {
+      'APIAPP_MODE': 'DOCKER',
+      'APIAPP_VERSION': 'TEST-3.3.3',
+      'APIAPP_FRONTEND': '../app',
+      'APIAPP_APIURL': 'http://apiurlxxx:45/aa/bb/cc',
+      'APIAPP_APIACCESSSECURITY': '[]',
+      'APIAPP_USERFORJOBS': 'dockjobuser',
+      'APIAPP_GROUPFORJOBS': 'dockjobgroup',
+    }
+    appObj.init(env)
+
+    cmdToExecute = 'whoami'
+    expResSTDOUT = 'dockjobuser\n'
+    res = appObj.jobExecutor.executeCommand(cmdToExecute)
+    self.assertEqual(res.stdout.decode(), expResSTDOUT)
+
