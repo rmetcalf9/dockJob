6e7fb983deaa92561ff602d01c7c56ee76dab8f0
Robert Metcalf <rmetcalf9@googlemail.com>
2018-03-26 15:12:46 +0100

    #16 Adding next job to run information

--- webfrontend/src/pages/Dashboard.vue
+++ webfrontend/src/pages/Dashboard.vue
@@ -10,6 +10,7 @@
           <tr><td align="right">Server Startup Time:</td><td>{{ serverInfo.Server.ServerStartupTime }}</td></tr>
           <tr><td align="right">Current Time on Server:</td><td>{{ serverInfo.Server.ServerDatetime }}</td></tr>
           <tr><td align="right">Total jobs setup:</td><td>{{ serverInfo.Jobs.TotalJobs }}</td></tr>
+          <tr><td align="right">Total jobs executed:</td><td>{{ serverInfo.Server.TotalJobExecutions }}</td></tr>
         </table>
       </q-card-main>
     </q-card>
@@ -20,7 +21,14 @@
         <span slot="subtitle">Next Job due to run</span>
       </q-card-title>
       <q-card-main>
-        Job Name, Date Time, How Long
+        <table>
+          <tr><td align="right">Name:</td><td>
+            <router-link :to="'/jobs/' + serverInfo.Jobs.NextJobsToExecute[0].guid" tag="a" class="text-grey-8">
+              {{ serverInfo.Jobs.NextJobsToExecute[0].name }}
+            </router-link>
+          </td></tr>
+          <tr><td align="right">When:</td><td>{{ serverInfo.Jobs.NextJobsToExecute[0].nextScheduledRun }}</td></tr>
+        </table>
       </q-card-main>
     </q-card>
 
