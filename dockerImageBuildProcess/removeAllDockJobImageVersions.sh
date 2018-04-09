fa84bacdb8320bedd6c811d7ddbef155f1f6644f
Robert Metcalf <rmetcalf9@googlemail.com>
2018-04-09 17:02:04 +0100

    Image removal script

--- dockerImageBuildProcess/removeAllDockJobImageVersions.sh
+++ dockerImageBuildProcess/removeAllDockJobImageVersions.sh
@@ -4,15 +4,28 @@ source ./setEnviroment.sh
 
 # Script designed to remove all local dockjob images
 
-#TODO Alter this code so it removes only metcarob/dockjob images
+ii=$(docker images | grep metcarob/dockjob)
+if [ 0 -eq ${#ii} ]; then
+  echo 'No metcarob/dockjob images present'
+  exit 0
+fi
 
-docker system prune -a -f
+docker images -a | grep "metcarob/dockjob" | awk '{print $3}' | xargs docker rmi -f
+#docker system prune -a -f
 RES=$?
 if [ ${RES} -ne 0 ]; then
   cd ${START_DIR}
   echo 'ERROR'
   exit 1
 fi
+
+ii=$(docker images | grep metcarob/dockjob)
+if [ 0 -ne ${#ii} ]; then
+  echo 'Error metcarob/dockjob images remain'
+  exit 1
+fi
+
+echo 'completed sucessfully'
 
 exit 0
 
