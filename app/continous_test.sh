6bf5b192b08c60a413d3f9f6d5199e0e679aa96b
Robert Metcalf <rmetcalf9@googlemail.com>
2018-04-23 09:27:42 +0100

    Only check particular dirs for files to change

--- app/continous_test.sh
+++ app/continous_test.sh
@@ -4,8 +4,8 @@ echo 'To test one file pass filename as first param'
 echo 'e.g. sudo ./continous_test.sh test_JobExecution.py'
 
 if [ $# -eq 0 ]; then
-  until ack -f --python | entr -d nosetests --rednose ./test; do sleep 1; done
+  until ack -f --python  ./src ./test | entr -d nosetests --rednose ./test; do sleep 1; done
 else
-  until ack -f --python | entr -d nosetests --rednose ./test/${1}; do sleep 1; done
+  until ack -f --python  ./src ./test | entr -d nosetests --rednose ./test/${1}; do sleep 1; done
 fi
 
