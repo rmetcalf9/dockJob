36d622222e73c30212d49012ecd1969fa310d1e1
Robert Metcalf <rmetcalf9@googlemail.com>
2018-05-11 10:15:50 +0100

    Extending to add a test job and execute the job manually a number of times

--- app/insert_example_jobs.sh
+++ app/insert_example_jobs.sh
@@ -9,6 +9,20 @@ do
   curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "${JSON}" 'http://127.0.0.1:80/api/jobs/'
 done
 
+echo 'Inserting Example Job for Executions'
 
+
+JSON="{    \"enabled\": false,    \"name\": \"Job With Executions\",    \"repetitionInterval\": \"\",    \"command\": \"ls -la\"  }"
+
+JWEGUID=$(curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "${JSON}" 'http://127.0.0.1:80/api/jobs/' | \
+    python -c "import sys, json; print json.load(sys.stdin)['guid']")
+
+echo "GUID for test executions: ${JWEGUID}"
+for i in `seq 0 12`;
+do
+  TN=$(printf "%03d" ${i})
+  JSON="{    \"name\": \"TestExecution ${TN}\"  }"
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "${JSON}" "http://127.0.0.1:80/api/jobs/${JWEGUID}/execution"
+done
 
 
