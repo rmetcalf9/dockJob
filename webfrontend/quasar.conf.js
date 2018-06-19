7fc6cef92906c49b9daf3774681b57ae8e950945
Robert Metcalf <rmetcalf9@googlemail.com>
2018-06-19 13:34:48 +0100

    Saving initial changes

--- webfrontend/quasar.conf.js
+++ webfrontend/quasar.conf.js
@@ -46,6 +46,7 @@ module.exports = function (ctx) {
     // framework: 'all' --- includes everything; for dev only!
     framework: {
       components: [
+        'QAutocomplete',
         'QCard',
         'QCardTitle',
         'QCardMain',
--- webfrontend/src/components/JobAutocomplete.vue
+++ webfrontend/src/components/JobAutocomplete.vue
@@ -6,11 +6,19 @@
       :float-label="floatlabel"
       :error-label="errorlabel"
       :error="invalid"
-    />
+    >
+      <q-autocomplete
+        @search="search"
+        :min-characters="0"
+        @selected="selected"
+      />
+    </q-input>
   </div>
 </template>
 
 <script>
+import globalStore from '../store/globalStore'
+import restcallutils from '../restcallutils'
 
 export default {
   props: [
@@ -23,6 +31,33 @@ export default {
     }
   },
   methods: {
+    search (terms, done) {
+      // make an AJAX call
+      // then call done(Array results)
+      var TTT = this
+      var callback = {
+        ok: function (response) {
+          // done(response.data.result.map(function (ite) {
+          //  return {
+          //    value: ite.guid,
+          //    label: ite.name
+          //  }
+          //}))
+          done([{value: 'a', label: 'b'}])
+        },
+        error: function (error) {
+          console.log('JobAutoComplete Error')
+          console.log(error)
+          done([])
+        }
+      }
+      var queryString = restcallutils.buildQueryString('jobs/', [])
+      // console.log(queryString)
+      globalStore.getters.apiFN('GET', queryString, undefined, callback)
+    },
+    selected (item) {
+      console.log('TODO Item selected ' + item)
+    },
     setValue (jobNAME) {
       this.$emit('modelupdate', {guid: 'TODO work out guid for job ' + jobNAME, name: jobNAME})
     }
