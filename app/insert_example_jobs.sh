192eb71728ca3c9841f4c69ece429b6cc5960238
Robert <rmetcalf9@googlemail.com>
2022-11-27 21:43:06 +0000

    Insert working on new port

--- app/insert_example_jobs.sh
+++ app/insert_example_jobs.sh
@@ -2,6 +2,8 @@
 
 echo 'Inserting Example Jobs'
 
+APIURL="http://127.0.0.1:8098/api"
+
 read -p "Continue (y/n)?" CONT
 if [ "$CONT" != "y" ]; then
   if [ "$CONT" != "Y" ]; then
@@ -12,13 +14,13 @@ fi
 sleep 5
 
 JSON="{    \"enabled\": false,    \"name\": \"SC Success\",    \"repetitionInterval\": \"\",    \"command\": \"ls -la\"  }"
-STATECHNAGESUCCESSJOBGUID=$(curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "${JSON}" 'http://127.0.0.1:80/api/jobs/' | \
+STATECHNAGESUCCESSJOBGUID=$(curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "${JSON}" ${APIURL}'/jobs/' | \
     python3 -c "import sys, json; print(json.load(sys.stdin)['guid'])")
 JSON="{    \"enabled\": false,    \"name\": \"SC Fail\",    \"repetitionInterval\": \"\",    \"command\": \"ls -la\"  }"
-STATECHNAGEFAILJOBGUID=$(curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "${JSON}" 'http://127.0.0.1:80/api/jobs/' | \
+STATECHNAGEFAILJOBGUID=$(curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "${JSON}" ${APIURL}'/jobs/' | \
     python3 -c "import sys, json; print(json.load(sys.stdin)['guid'])")
 JSON="{    \"enabled\": false,    \"name\": \"SC Unknown\",    \"repetitionInterval\": \"\",    \"command\": \"ls -la\"  }"
-STATECHNAGEUNKNOWNJOBGUID=$(curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "${JSON}" 'http://127.0.0.1:80/api/jobs/' | \
+STATECHNAGEUNKNOWNJOBGUID=$(curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "${JSON}" ${APIURL}'/jobs/' | \
     python3 -c "import sys, json; print(json.load(sys.stdin)['guid'])")
 
 
@@ -27,7 +29,7 @@ for i in `seq 0 59`;
 do
   TN=$(printf "%03d" ${i})
   JSON="{    \"enabled\": true,    \"name\": \"Test ${TN}\",    \"repetitionInterval\": \"HOURLY:${i}\",    \"command\": \"echo Running Test ${TN}\necho Finished\",  \"pinned\": true ,  \"StateChangeSuccessJobGUID\": \"${STATECHNAGESUCCESSJOBGUID}\", \"StateChangeFailJobGUID\": \"${STATECHNAGEFAILJOBGUID}\",   \"StateChangeUnknownJobGUID\": \"${STATECHNAGEUNKNOWNJOBGUID}\" }"
-  curl --fail -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "${JSON}" 'http://127.0.0.1:80/api/jobs/'
+  curl --fail -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "${JSON}" ${APIURL}'/jobs/'
   RES=$?
   echo $RES
   if [[ ${RES} -ne 0 ]]; then
@@ -42,7 +44,7 @@ echo 'Inserting Example Job for Executions'
 
 JSON="{    \"enabled\": false,    \"name\": \"Job With Executions\",    \"repetitionInterval\": \"\",    \"command\": \"ls -la\"  }"
 
-JWEGUID=$(curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "${JSON}" 'http://127.0.0.1:80/api/jobs/' | \
+JWEGUID=$(curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d "${JSON}" ${APIURL}'/jobs/' | \
     python3 -c "import sys, json; print(json.load(sys.stdin)['guid'])")
 
 echo "GUID for test executions: ${JWEGUID}"
