c042179fe823958a2febf2b6d87ecb9f4ece82e0
Robert <rmetcalf9@googlemail.com>
2023-03-17 13:25:57 +0000

    fix error in healthcheck

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-curl -f http://127.0.0.1:80/public/web/frontend/index.html?healthcheck=true
+curl -f http://127.0.0.1:80/frontend/index.html?healthcheck=true
 RES=$?
 if [ ${RES} -ne 0 ]; then
   echo "healthcheck.sh - failed quasar index"
   exit 1
 fi
 
-curl -f http://127.0.0.1:80/public/api/info/serverinfo?healthcheck=true
+curl -f http://127.0.0.1:80/api/serverinfo?healthcheck=true
 RES=$?
 if [ ${RES} -ne 0 ]; then
   echo "healthcheck.sh - failed python app"
